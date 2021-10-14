1b5f0e14e55aff560cb31c34cf3a4150a3707783
LuX Developer <87079094+muzrblx@users.noreply.github.com>
2021-10-14 17:06:42 -0400

    Create hub.lua

---
+++ hub.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 4520749081 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/KingLegacy.lua'))()
+elseif game.PlaceId == 6751580221 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/VerdantMoon2'))()
+elseif game.PlaceId == 7429434108 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/animetappers.lua'))()
+else
+    v:Kick("Game not supported")
+end
+    
