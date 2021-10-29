36654e2f235ddb32972176ce173aa6575020f22b
LuX Developer <87079094+muzrblx@users.noreply.github.com>
2021-10-29 16:32:58 -0700

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -4,7 +4,19 @@ elseif game.PlaceId == 6751580221 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/VerdantMoon2'))()
 elseif game.PlaceId == 7429434108 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/animetappers.lua'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/ADumbDay'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/blackgrimore'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/animedestroyersimulator.lua'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/blackgrimore'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/fishingsimulator'))()
+elseif game.PlaceId == 0 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/untamedplanet'))()
 else
-    v:Kick("Game not supported")
+    setclipboard("https://discord.gg/f78rduRP")
+    v:Kick("Not a supported game sorry, discord copied to clipboard")
 end
-    
